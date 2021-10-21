02a5530f357ea3be4f88c044689d6de78cf4e34a
Iskander <aldr046@mail.ru>
2021-10-21 10:42:27 +0400

    добавлена папка unit-тестов подключен nuGet-пакет unit-тестов добавлен класс тестирования FolderItemView

---
+++ PlaylistMaker/UnitTests/Test_FolderItemViev.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlaylistMaker.Contexts;
+using PlaylistMaker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistMaker.UnitTests
+{
+    /// <summary>
+    /// Тестирование класса FolderItemView
+    /// </summary>
+    [TestClass]
+    public class Test_FolderItemViev
+    {
+        /// <summary>
+        /// Тестирование статуса Exist
+        /// </summary>
+        [TestMethod]
+        public void Test_Status_IsExist()
+        {
+            FolderItemView folderItemView = new FolderItemView(@"E:\Music\", @"E:\Music\Рок\", false);
+            Assert.AreEqual(ItemStatus.Exist, folderItemView.Status);
+        }
+    }
+}
