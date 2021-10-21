05c731bb625f3a14bf875fa43457ec9810483d45
Iskander <aldr046@mail.ru>
2021-10-21 09:11:18 +0400

    Добавлен проект конструктора плей-листа для Renault EasyLink

---
+++ PlaylistMaker/FileItemView.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistMaker
+{
+    internal class FileItemView : INotifyPropertyChanged
+    {
+        private string fullName = string.Empty;
+        /// <summary>
+        /// Полное имя файла
+        /// </summary>
+        internal string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if(fullName != value)
+                {
+                    fullName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string fileName = string.Empty;
+        /// <summary>
+        /// Имя файла
+        /// </summary>
+        internal string FileName
+        {
+            get => fileName;
+            private set
+            {
+                if(fileName != value)
+                {
+                    fileName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string folderName = string.Empty;
+        /// <summary>
+        /// Имя директории
+        /// </summary>
+        internal string FolderName
+        {
+            get => folderName;
+            private set
+            {
+                if (folderName != value)
+                {
+                    folderName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private int duration = 0;
+        /// <summary>
+        /// Длительность
+        /// </summary>
+        internal int Duration
+        {
+            get => duration;
+            private set
+            {
+                if(duration != value)
+                {
+                    duration = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Изменение свойства
+        /// </summary>
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ PlaylistMaker/MainWindowContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistMaker
+{
+    /// <summary>
+    /// Контекст данных главного окна
+    /// </summary>
+    internal class MainWindowContext : INotifyPropertyChanged
+    {
+        private string folderForPlaylist = string.Empty;
+        /// <summary>
+        /// Путь к папке плейлиста
+        /// </summary>
+        internal string FolderForPlayList
+        {
+            get => folderForPlaylist;
+            set
+            {
+                if(folderForPlaylist != value)
+                {
+                    folderForPlaylist = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string playlistFileName = string.Empty;
+        /// <summary>
+        /// Имя файла плей-листа
+        /// </summary>
+        internal string PlaylistFileName
+        {
+            get => playlistFileName;
+            private set
+            {
+                if(playlistFileName != value)
+                {
+                    playlistFileName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Изменение свойства
+        /// </summary>
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
